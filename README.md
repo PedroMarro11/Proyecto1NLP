813c86bac308c20812cdd6c0f50525e6f5ee97cb
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2025-10-03 22:31:59 -0600

    Preprocesamiento completado

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Datos:
 https://www.kaggle.com/datasets/javieroterovizoso/spanish-political-fake-news
 
 
+preprocesamiento.py:
+Funciones necesarias para preprocesar un corpus, incluyendo el algoritmo de Levenshtein.
---
+++ preprocesamiento.py
@@ -0,0 +1,63 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import string
+import Levenshtein as lev
+
+# Descargar recursos necesarios de NLTK
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Inicializar herramientas de NLTK
+stop_words = set(stopwords.words('spanish'))
+
+def preprocesar_texto(corpus: str, vocabulario: list) -> list:
+    """
+    Preprocesa un texto:
+    - Convierte a minúsculas
+    - elimina stopwords
+    - Elimina puntuación
+    - reemplaza caracteres especiales
+    - Stemming
+    - Lematización
+    - Tokenización
+    - algoritmo de Levenshtein para identificar palabras similares en el vocabulario dado
+    Args:
+        corpus (str): El texto a preprocesar.
+        vocabulario (list): Lista de palabras para comparar usando Levenshtein.
+    Returns:
+        list: Lista de tokens preprocesados.
+    """
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+
+    # Convertir a minúsculas
+    corpus = corpus.lower()
+
+    #reemplazar tildes y caracteres especiales
+    corpus = corpus.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
+    corpus = corpus.replace('ñ', 'n').replace('ü', 'u').replace('ç', 'c')
+
+    # Tokenización
+    tokens = word_tokenize(corpus, language='spanish')
+
+    # Eliminar puntuación y stopwords, aplicar stemming y lematización, y usar Levenshtein
+    processed_tokens = []
+    for token in tokens:
+        if token not in string.punctuation and token not in stop_words:
+            stemmed = stemmer.stem(token)
+            lemmatized = lemmatizer.lemmatize(stemmed)
+            processed_tokens.append(lemmatized)
+            if vocabulario:
+                for palabra in vocabulario:
+                    if lev.distance(lemmatized, palabra) <= 1:
+                        processed_tokens.append(palabra)
+                    else:
+                        processed_tokens.append(lemmatized)
+                        vocabulario.append(lemmatized)
+            else:
+                processed_tokens.append(lemmatized)
+                vocabulario.append(lemmatized)
+    return processed_tokens
